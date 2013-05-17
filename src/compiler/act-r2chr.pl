b2010f99079f7073dd8714968575d5115659391d
Daniel Gall <dgall@isobeef.org>
2013-05-17 14:18:41 +0200

    added cut (!) operator to avoid backtracking in compiler.

--- src/compiler/act-r2chr.pl
+++ src/compiler/act-r2chr.pl
@@ -15,12 +15,11 @@ readAll(S,[C|Cs]) :-
 compile_file(F) :-
   open(F,read,S),
   readAll(S,Cs),
-  compile(Cs,Structure),
-  !,
+  compile(Cs,Structure), !,
   %display(Structure),
   console,
   nl,nl,
-  compile_structure(Structure),
+  compile_structure(Structure), !,
   close(S).
 
 compile(X,S) :- getTokens(X,T), parse(T,S), write(S).
