bd09a19951de1d893987c35acd5598d51f7b8e43
Daniel Gall <dgall@isobeef.org>
2013-05-22 15:11:57 +0200

    added compiling for negated slots and function calls.

--- src/compiler/act-r2chr.pl
+++ src/compiler/act-r2chr.pl
@@ -2,7 +2,7 @@
 :- use_module(tokenizer).
 :- use_module(parser).
 
-:- chr_constraint spypoint/0, chr/5,chrl/5, list2goal/2, file/1, stream/2, console/0, line/1,compile_structure/1,compile_structure2/2,compile_structure3/3,symbol_table/2, end_of_block/0.
+:- chr_constraint spypoint/0, chr/5,chrl/5, list2goal/2, file/1, stream/2, console/0, line/1,compile_structure/1,compile_structure2/2,compile_structure2_lhs/3,compile_structure3/3,compile_structure3_lhs/4,symbol_table/2, end_of_block/0.
 
 readAll(S, []) :-
   at_end_of_stream(S).
@@ -30,27 +30,94 @@ compile_file(F) :-
 compile_structure(s(S)) <=> compile_structure(S).
 compile_structure(s(S, Ss)) <=> compile_structure(S), compile_structure(Ss).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Production rule compilation %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 compile_structure(production_rule(production_name(Name), LHS, RHS)) <=> 
-  compile_structure2(LHS, Head), 
+  compile_structure2_lhs(LHS, Head, Guard), 
   compile_structure2(RHS, Body),
   end_of_block,
-  chrl(Name, Head,[],[],Body).
+  chrl(Name, Head,[],Guard,Body).
 
-compile_structure2(lhs(BufferTests),R) <=>
-  compile_structure2(BufferTests,R).
+% LHS
+%%  
+
+compile_structure2_lhs(lhs(BufferTests),R,Guard) <=>
+  compile_structure2_lhs(BufferTests,R, Guard).
   
-compile_structure2(buffer_tests(BufferTest),R) <=>
-  compile_structure2(BufferTest, R).
+compile_structure2_lhs(buffer_tests(BufferTest),R,Guard) <=>
+  compile_structure2_lhs(BufferTest, R, Guard).
 
 
-compile_structure2(buffer_tests(BufferTest, Next),R) <=> 
-  compile_structure2(BufferTest, RBufferTest),
-  compile_structure2(Next, RNext),
-  append(RBufferTest,RNext,R).
+compile_structure2_lhs(buffer_tests(BufferTest, Next),R,Guard) <=> 
+  compile_structure2_lhs(BufferTest, RBufferTest,Guard1),
+  compile_structure2_lhs(Next, RNext,Guard2),
+  append(RBufferTest,RNext,R),
+  append(Guard1,Guard2,Guard).
   
-compile_structure2(buffer_test(buffer(Buffer), SlotTests),R) <=>
-  compile_structure3(SlotTests, Chunk, R1),
+compile_structure2_lhs(buffer_test(buffer(Buffer), SlotTests),R, Guard) <=>
+  compile_structure3_lhs(SlotTests, Chunk, R1,Guard),
   R = [buffer(Buffer,Chunk)|R1].
+  
+% 
+% Compile slot_tests and slot_test
+%
+
+% structure: single slot_test in slot_tests-environment.
+% returns: list containing that slot_test in form of a chunk_has_slot constraint
+compile_structure3_lhs(slot_tests(SlotTest), Chunk, R, Guard) <=>
+  compile_structure3_lhs(SlotTest, Chunk, R, Guard).
+
+% structure: slot_test followed by next slot_tests in slot_tests-environment
+% returns: list containing all the slot_tests in form of chunk_has_slot constraints
+compile_structure3_lhs(slot_tests(SlotTest, Next), Chunk, R, Guard) <=>
+  compile_structure3_lhs(SlotTest, Chunk, RSlotTest, Guard1),
+  compile_structure3_lhs(Next, Chunk, RNext, Guard2),
+  append(RSlotTest,RNext,R),
+  append(Guard1,Guard2,Guard).
+
+% structure: slot_test containing one slot-value-pair with slot S and value V
+% returns: a list containing this one slot_test in form of a chunk_has_slot constraint  
+compile_structure3_lhs(slot_test(slot_value_pair(slot(S),value(V))), Chunk, R,Guard) <=>
+  R = [chunk_has_slot(Chunk,S,V)],
+  Guard = [].
+
+% structure: slot_test containing one slot-variable-pair with slot S and variable V
+% If the variable V already has been used in this rule, it will be bound to that former variable.
+% returns: a list containing this one slot_test in form of a chunk_has_slot constraint  
+symbol_table(V, Var) \ compile_structure3_lhs(slot_test(slot_variable_pair(slot(S),variable(V))), Chunk, R, Guard) <=>
+  R = [chunk_has_slot(Chunk,S,Var)],
+  Guard = [].
+  
+compile_structure3_lhs(slot_test(slot_variable_pair(slot(S),variable(V))), Chunk, R, Guard) <=>
+  symbol_table(V, Var),
+  R = [chunk_has_slot(Chunk,S,Var)],
+  Guard = [].
+
+% negated slot tests
+%
+  
+% structure: neg_slot_test containing one slot-value-pair with slot S and value V
+% returns: a list containing this one slot_test in form of a chunk_has_slot constraint  
+compile_structure3_lhs(neg_slot_test(slot_value_pair(slot(S),value(V))), Chunk, R,Guard) <=>
+  R = [chunk_has_slot(Chunk,S,X)],
+  Guard = [X \== V].
+
+% structure: slot_test containing one slot-variable-pair with slot S and variable V
+% If the variable V already has been used in this rule, it will be bound to that former variable.
+% returns: a list containing this one slot_test in form of a chunk_has_slot constraint  
+symbol_table(V, Var) \ compile_structure3_lhs(neg_slot_test(slot_variable_pair(slot(S),variable(V))), Chunk, R, Guard) <=>
+  R = [chunk_has_slot(Chunk,S,X)],
+  Guard = [X \== Var].
+  
+compile_structure3_lhs(neg_slot_test(slot_variable_pair(slot(S),variable(V))), Chunk, R, Guard) <=>
+  symbol_table(V, Var),
+  R = [chunk_has_slot(Chunk,S,X)],
+  Guard = [X \== Var].
+  
+% RHS
+%%    
   
 compile_structure2(rhs(RHS),R) <=> 
   compile_structure2(RHS,R).
@@ -149,36 +216,6 @@ compile_structure2(slot_rhs(slot_variable_pair(slot(S),variable(V))),R) <=>
   symbol_table(V, Var),
   R = [(S,Var)].
    
-% 
-% Compile slot_tests and slot_test
-%
-
-% structure: single slot_test in slot_tests-environment.
-% returns: list containing that slot_test in form of a chunk_has_slot constraint
-compile_structure3(slot_tests(SlotTest), Chunk, R) <=>
-  compile_structure3(SlotTest, Chunk, R).
-
-% structure: slot_test followed by next slot_tests in slot_tests-environment
-% returns: list containing all the slot_tests in form of chunk_has_slot constraints
-compile_structure3(slot_tests(SlotTest, Next), Chunk, R) <=>
-  compile_structure3(SlotTest, Chunk, RSlotTest),
-  compile_structure3(Next, Chunk, RNext),
-  append(RSlotTest,RNext,R).
-
-% structure: slot_test containing one slot-value-pair with slot S and value V
-% returns: a list containing this one slot_test in form of a chunk_has_slot constraint  
-compile_structure3(slot_test(slot_value_pair(slot(S),value(V))), Chunk, R) <=>
-  R = [chunk_has_slot(Chunk,S,V)].
-
-% structure: slot_test containing one slot-variable-pair with slot S and variable V
-% If the variable V already has been used in this rule, it will be bound to that former variable.
-% returns: a list containing this one slot_test in form of a chunk_has_slot constraint  
-symbol_table(V, Var) \ compile_structure3(slot_test(slot_variable_pair(slot(S),variable(V))), Chunk, R) <=>
-  R = [chunk_has_slot(Chunk,S,Var)].
-  
-compile_structure3(slot_test(slot_variable_pair(slot(S),variable(V))), Chunk, R) <=>
-  symbol_table(V, Var),
-  R = [chunk_has_slot(Chunk,S,Var)].
 
   
 %
