23d305aa25d75f4c18fcc788e03fb02eca91c422
Daniel Gall <dgall@isobeef.org>
2013-06-10 15:29:04 +0200

    added simple priority queue for handling time scheduling.

---
+++ src/core/scheduler.pl
@@ -0,0 +1,34 @@
+:- use_module(library(chr)).
+
+
+% very simple priority queue
+% sorts time-events pairs by time
+% if two events have the same time, they will be set in order in unpredictable order
+
+:- op(900, xfx, ['-->']).
+  
+:- chr_constraint (-->)/2, add_q/2, de_q/1.
+
+add_q(Time,Evt) <=>
+  s --> q(Time,Evt).
+
+A --> A <=> true.
+
+A --> B, A --> C <=>
+  leq(B,C) |
+  A --> B,
+  B --> C.  
+  
+  
+leq(s,_).
+leq(q(Time1,_), q(Time2,_)) :-
+  Time1 =< Time2.
+  
+de_q(X), s --> A, A --> B <=>
+  X = A,
+  s --> B.
+
+de_q(X), s --> A <=>
+  X = A.
+  
+  
