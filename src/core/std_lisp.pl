0c9f8212f86a3d609ecd7b9b67c0c15c213c995a
Daniel Gall <dgall@isobeef.org>
2013-06-13 10:01:47 +0200

    bugfix: calling add_dm in implementation of lisp_adddm. Reason: with add_chunk, chunks were added to buffer system, not to declarative memory.

--- src/core/std_lisp.pl
+++ src/core/std_lisp.pl
@@ -1,5 +1,5 @@
 :- use_module(library(chr)).
-:- chr_constraint lisp_chunktype/1,lisp_adddm/1.
+:- chr_constraint lisp_chunktype/1,lisp_adddm/1,lisp_goalfocus/1.
 
 lisp_chunktype([Type|Slots]) <=>
   add_chunk_type(Type,Slots).
@@ -8,7 +8,7 @@ lisp_adddm([]) <=> true.
   
 lisp_adddm([C|Cs]) <=>
   gen_chunk(C,Chunk),
-  add_chunk(Chunk),
+  add_dm(Chunk),
   lisp_adddm(Cs).
   
 gen_chunk([Name,isa,Type|Slots],Res) :-
