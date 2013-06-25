aa6623dfc602a5edece7212cde35c1674f838803
Daniel Gall <dgall@isobeef.org>
2013-06-25 16:33:02 +0200

    added compilable addition example.

--- src/compiler/addition.lisp
+++ src/compiler/addition.lisp
@@ -1,6 +1,24 @@
-(p initialize-addition
+(define-model addition
+
+(chunk-type count-order first second)
+(chunk-type add arg1 arg2 sum count)
+
+(add-dm
+   (a ISA count-order first 0 second 1)
+   (b ISA count-order first 1 second 2)
+   (c ISA count-order first 2 second 3)
+   (d ISA count-order first 3 second 4)
+   (e ISA count-order first 4 second 5)
+   (f ISA count-order first 5 second 6)
+   (g ISA count-order first 6 second 7)
+   (h ISA count-order first 7 second 8)
+   (i ISA count-order first 8 second 9)
+   (j ISA count-order first 9 second 10)
+   (second-goal ISA add arg1 5 arg2 2))
+
+(P initialize-addition
    =goal>
-      isa         add
+      ISA         add
       arg1        =num1
       arg2        =num2
       sum         nil
@@ -13,9 +31,9 @@
       first      =num1
 )
 
-(p terminate-addition
+(P terminate-addition
    =goal>
-      isa         add
+      ISA         add
       count       =num
       arg2        =num
       sum         =answer
@@ -24,13 +42,13 @@
       count       nil
 )
 
-(p increment-count
+(P increment-count
    =goal>
-      isa         add
+      ISA         add
       sum         =sum
       count       =count
    =retrieval>
-      isa         count-order
+      ISA         count-order
       first       =count
       second      =newcount
 ==>
@@ -41,14 +59,14 @@
       first      =sum
 )
 
-(p increment-sum
+(P increment-sum
    =goal>
-      isa         add
+      ISA         add
       sum         =sum
       count       =count
     - arg2        =count
    =retrieval>
-      isa         count-order
+      ISA         count-order
       first       =sum
       second      =newsum
 ==>
@@ -57,4 +75,7 @@
    +retrieval>
       isa        count-order
       first      =count
+)
+
+(goal-focus second-goal)
 )
