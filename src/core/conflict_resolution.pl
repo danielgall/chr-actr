29136e84eb0e7600270c4f245939d0c31940c483
Daniel Gall <daniel.gall@uni-ulm.de>
2014-01-23 13:29:24 +0100

    fixed bug in reward-based conflict resolution: productions are rewarded in correct order now.

--- src/core/conflict_resolution.pl
+++ src/core/conflict_resolution.pl
@@ -2,10 +2,16 @@
 % Production Utilities
 %
 
-:- chr_constraint set_production_utility/2, production_utility/2, set_default_utilities/1, conflict_set/1, choose/0, apply_rule/1, reward/2, set_reward/2, trigger_reward/1, to_reward/2.
+round(X,Precision,X1) :-
+  P = 10^Precision,
+  X1 is round(X*P)/P.      
+
+:- chr_constraint set_production_utility/2, production_utility/2, set_default_utilities/1, conflict_set/1, choose/0, apply_rule/1, reward/2, set_reward/2, trigger_reward/1, to_reward/1.
 
 set_production_utility(P,U), production_utility(P,_) <=> 
-  production_utility(P,U).
+  round(U,6,U1),
+  production_utility(P,U1),
+  write('setting utility of '),write(P),write(' to '),writeln(U1).
   
 set_production_utility(P,U) <=> 
   production_utility(P,U).
@@ -43,7 +49,7 @@ choose @ choose, conflict_set(P) <=>
   
 apply_rule(rule(P,_,_)) ==> P \== [] | write('firing rule '),write(P),nl.
 
-apply_rule(rule(P,_,_)) ==> P \== [] | get_now(Now), FireTime is Now-0.05,to_reward(P,FireTime).
+apply_rule(rule(P,_,_)) ==> P \== [] | get_now(Now), FireTime is Now-0.05,to_reward([(P,FireTime)]).
 
 apply_rule(rule(P,_,_)), reward(P,R) ==>
   P \== [] |
@@ -60,15 +66,32 @@ set_reward(P,R), reward(P,_) <=>
   
 set_reward(P,R) <=>
   reward(P,R).
+
+  
+to_reward(L1), to_reward(L2) <=>
+  merge(L1,L2,L),
+  to_reward(L).
+  
+merge([],L,L).
+merge(L,[],L).
+merge([(P1,T1)|R1],[(P2,T2)|R2],Res) :-
+  T1 =< T2,
+  merge(R1,[(P2,T2)|R2],L),
+  Res = [(P1,T1)|L].
+merge([(P1,T1)|R1],[(P2,T2)|R2],Res) :-
+  T1 > T2,
+  merge([(P1,T1)|R1],R2,L),
+  Res = [(P2,T2)|L].
     
-trigger_reward(R), production_utility(P,U) \ to_reward(P,FireTime) <=>
+trigger_reward(R), production_utility(P,U) \ to_reward([(P,FireTime)|Rest]) <=>
   calc_reward(R,FireTime,Reward),
   get_conf(alpha,Alpha),
   NewU is U + Alpha*(Reward-U),
   set_production_utility(P,NewU),
   write('triggered reward for rule: '),write(P),nl,
-  write('U(n-1) = '),write(U),write(' R(n) = '),write(R),write(' ('),write(R),write(' - '),get_now(Now),Diff is R - (Now-FireTime),write(Diff),write(')'),nl,
-  write('U(n) = '),write(NewU),nl.
+  write('U(n-1) = '),write(U),write(' R(n) = '),write(Reward),write(' ('),write(R),write(' - '),get_now(Now),Diff is Now-FireTime,write(Diff),write(')'),nl,
+  write('U(n) = '),write(NewU),nl,
+  to_reward(Rest).
   
 calc_reward(R,FireTime,Reward) :-
   get_now(Now),
